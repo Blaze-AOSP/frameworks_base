c4dba9157627fd6be0a7e237c4a064ac7a5ddb93
bigrushdog <randall.rushing@gmail.com>
2017-09-12 14:14:35 +0800

    Add api to take screenshots
    abc: extracted from DUI
    add a public call to ArrowUtils
    
    Change-Id: I7110a90b84acd7cd53e0646ebbc441c5b180efd3

--- core/java/com/android/internal/util/blaze/BlazeUtils.java
+++ core/java/com/android/internal/util/blaze/BlazeUtils.java
@@ -17,6 +17,7 @@
 package com.android.internal.util.blaze;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.input.InputManager;
 import android.os.Looper;
@@ -24,6 +25,8 @@ import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemClock;
+import android.view.IWindowManager;
+import android.view.WindowManagerGlobal;
 import android.view.InputDevice;
 import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
@@ -39,6 +42,9 @@ import com.android.internal.statusbar.IStatusBarService;
  * Some custom utilities
  */
 public class BlazeUtils {
+
+    public static final String INTENT_SCREENSHOT = "action_handler_screenshot";
+    public static final String INTENT_REGION_SCREENSHOT = "action_handler_region_screenshot";
 
     public static void switchScreenOff(Context ctx) {
         PowerManager pm = (PowerManager) ctx.getSystemService(Context.POWER_SERVICE);
@@ -97,6 +103,15 @@ public class BlazeUtils {
                         InputManager.INJECT_INPUT_EVENT_MODE_ASYNC);
             }
         }, 20);
+    }
+
+    public static void takeScreenshot(boolean full) {
+        IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
+        try {
+            wm.sendCustomAction(new Intent(full? INTENT_SCREENSHOT : INTENT_REGION_SCREENSHOT));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 
     private static final class FireActions {
